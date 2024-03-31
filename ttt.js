0676bc8b6e5793102190955745d70564a5b8233a
Nabajyoti Das <165571103+njd-12@users.noreply.github.com>
2024-03-31 20:36:01 +0530

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -8,9 +8,9 @@ let moves = 0;
 let winner = null;
 
 const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-    [0, 4, 8], [2, 4, 6]             // diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]             
 ];
 
 cells.forEach(cell => cell.addEventListener('click', handleCellClick));
